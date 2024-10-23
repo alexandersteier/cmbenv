28e3479b7e150e68df2e84ff8fe5cb1eb6399f09
Alexander Steier <131294913+alexandersteier@users.noreply.github.com>
2024-10-23 13:28:06 -0700

    Update pip-pkgs.sh
    Includes PyMC

--- conf/pip-pkgs.sh
+++ conf/pip-pkgs.sh
@@ -5,6 +5,7 @@ echo Installing pip packages at $(date)
 pip install --no-cache-dir "jax[cuda12]" "jaxlib" -f https://storage.googleapis.com/jax-releases/jax_cuda_releases.html
 pip install numpyro[cuda]
 pip install blackjax
+pip install pymc
 
 # see https://docs.nersc.gov/development/languages/python/parallel-python/
 pip install --force --no-cache-dir --no-binary=mpi4py mpi4py
