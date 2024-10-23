fb1aa79a7602986f0f30e7fa1db0c1b1bbbe440a
Alexander Steier <131294913+alexandersteier@users.noreply.github.com>
2024-10-23 14:01:10 -0700

    Update conda-pkgs.sh

--- conf/conda-pkgs.sh
+++ conf/conda-pkgs.sh
@@ -9,7 +9,6 @@ conda config --set solver libmamba
 conda config --set channel_priority flexible
 conda install --yes -c conda-forge -c anaconda -c nvidia -c defaults \
     astropy \
-    blackjax \
     camb \
     cuda-nvcc \
     cudatoolkit \
@@ -24,11 +23,9 @@ conda install --yes -c conda-forge -c anaconda -c nvidia -c defaults \
     jupyter \
     matplotlib \
     numpy \
-    numpyro \
     scipy \
     numba \
     pygtc \
-    pymc \
     pysm3 \
     namaster \
     tqdm \
